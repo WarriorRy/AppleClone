69b29e0fa094467963c217176394e24ec364a5aa
RiteshKumar <127754833+WarriorRy@users.noreply.github.com>
2023-07-10 10:50:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var slides = document.getElementsByClassName("slide");
 var currentSlide = 0;
-var slideInterval = setInterval(nextSlide, 2000);
+var slideInterval = setInterval(nextSlide, 10000);
 
 function nextSlide() {
   slides[currentSlide].style.display = "none";
